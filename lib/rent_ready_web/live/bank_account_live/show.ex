571671590c194509c196e1c37f00d512bf17f2f4
timwis <tim@timwis.com>
2023-11-17 13:07:56 +0000

    Use async+stream to render transactions

--- lib/rent_ready_web/live/bank_account_live/show.ex
+++ lib/rent_ready_web/live/bank_account_live/show.ex
@@ -2,6 +2,7 @@ defmodule RentReadyWeb.BankAccountLive.Show do
   use RentReadyWeb, :live_view
 
   alias RentReady.Banking
+  alias Phoenix.LiveView.AsyncResult
 
   @impl true
   def mount(%{"id" => id}, _session, socket) do
@@ -16,13 +17,36 @@ defmodule RentReadyWeb.BankAccountLive.Show do
      |> assign(:page_title, "Show Bank account")
      |> assign(:bank_connection, bank_account.bank_connection)
      |> assign(:bank_account, bank_account)
-     |> assign_async(:transactions, fn ->
-       case Banking.get_transactions(bank_account, from, to) do
-         {:ok, transactions} -> {:ok, %{transactions: transactions}}
-         {:error, reason} -> {:error, reason}
-       end
      |> assign(:changeset, Banking.change_transaction_selection())
+     |> stream(:transactions, [])
+     |> assign(:transactions_status, AsyncResult.loading())
+     |> start_async(:get_transactions, fn ->
+       Banking.fetch_remote_transactions(bank_account, from, to)
      end)}
+  end
+
+  def handle_async(:get_transactions, {:ok, {:ok, transactions}}, socket) do
+    %{transactions_status: transactions_status} = socket.assigns
+
+    {:noreply,
+     socket
+     |> assign(:transactions_status, AsyncResult.ok(transactions_status, true))
+     |> stream(:transactions, transactions)}
+  end
+
+  def handle_async(:get_transactions, {:ok, {:error, reason}}, socket) do
+    handle_async(:get_transactions, {:exit, reason}, socket)
+  end
+
+  def handle_async(:get_transactions, {:exit, reason}, socket) do
+    %{transactions_status: transactions_status} = socket.assigns
+
+    {:noreply,
+     assign(
+       socket,
+       :transactions_status,
+       AsyncResult.failed(transactions_status, {:exit, reason})
+     )}
   end
 
   @impl true
